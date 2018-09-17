26dd1c9fe2b24fd0ffffc2a76344905449616ad2
leesuyeong <super2298@naver.com>
2018-09-17 10:01:19 +0900

    project commit message

---
+++ p0917/src/p0917/HelloWorld.java
@@ -0,0 +1,12 @@
+package p0917;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		
+		
+	}
+
+}
