4bfada73fdfb1024927d17db57c3bc408d44e820
leesuyeong <super2298@naver.com>
2018-09-17 10:17:37 +0900

    p0917 update message

--- p0917/src/p0917/HelloWorld.java
+++ p0917/src/p0917/HelloWorld.java
@@ -5,8 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
-		
-		
+		System.out.println("헬로월드!!");
+		System.out.println("헬로월드2!!");
 	}
 
 }
